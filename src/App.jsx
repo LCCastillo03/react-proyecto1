f484d9775a56b0793308b14aa0dc8283564b3e9a
Lena Castillo <lenac@uninorte.edu.co>
2024-09-11 12:59:05 -0500

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
+import Header from './Componentes/Componentes/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {<div>
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div>}
+
+      <header>
+        <Header />
+      </header>
+      <h1 className='fondo_texto'>Cachon</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/Componentes/Componentes/header.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="p-6 bg-gray-100">
+      <header className="flex justify-center">
+        <img src="/gallery/logo.png" alt="Logo Image" className="w-32 h-auto" />
+      </header>
+    </div>
+  );
+}
+
+export default Header;
+
