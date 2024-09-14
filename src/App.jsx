f362555e4d9ac2dc21092b3f0cc3b2e9df09dedf
Lena Castillo <lenac@uninorte.edu.co>
2024-09-14 11:09:09 -0500

    capital

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './index.css'
-import Header from './Componentes/Componentes/Header'
-import Intro from './Componentes/Componentes/Intro'
-import Diagnostico from './Componentes/Componentes/Diagnostic'
-import Identidad from './Componentes/Componentes/Identidad'
-import ObjetivosEstrategicos from './Componentes/Componentes/Objetivos'
-import Footer from './Componentes/Componentes/Footer'
+import Header from './Componentes/Componentes/HeaderMod'
+import Intro from './Componentes/Componentes/IntroMod'
+import Diagnostico from './Componentes/Componentes/DiagnosticoMod'
+import Identidad from './Componentes/Componentes/IdentidadMod'
+import ObjetivosEstrategicos from './Componentes/Componentes/ObjetivosMod'
+import Footer from './Componentes/Componentes/FooterMod'
 
 
 function App() {
